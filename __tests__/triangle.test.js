ffc5edc39692d6c4cf0373282a60bfa72809fb1b
Richelle <richelle.ak@gmail.com>
2022-12-12 21:20:02 -0800

    pass first test

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,16 +1,36 @@
-import Triangle from './../src/triangle.js';
+import ThreeBy3 from './../src/sudoku.js';
 
-describe('Triangle', () => {
 
-  test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
-  });
-
-  test('should correctly determine whether three lengths are not a triangle', () =>{
-    const notTriangle = new Triangle (3,9,22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
+describe ("ThreeBy3", () => {
+  test("should correctly create an threeby3 object with 9 box values", () =>{
+    const threeby3 = new ThreeBy3(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    expect(threeby3.box1).toEqual(1);
+    expect(threeby3.box2).toEqual(2);
+    expect(threeby3.box3).toEqual(3);
+    expect(threeby3.box4).toEqual(4);
+    expect(threeby3.box5).toEqual(5);
+    expect(threeby3.box6).toEqual(6);
+    expect(threeby3.box7).toEqual(7);
+    expect(threeby3.box8).toEqual(8);
+    expect(threeby3.box9).toEqual(9);
   })
-});
+})
+
+// ThreeBy3.prototype.createArray = function () {
+//   this.arrayOfValues.splice(0, 8);
+//   let arr = Object.values(this);
+//   arr.pop();
+//   this.arrayOfValues.push(...arr);
+// };
+
+// ThreeBy3.prototype.findDuplicate = function () {
+//   let valuesAlreadySeen = []
+//   for (let i = 0; i < this.arrayOfValues.length; i++) {
+//     let value = this.arrayOfValues[i]
+//     if (valuesAlreadySeen.indexOf(value) !== -1) {
+//       return true
+//     }
+//     valuesAlreadySeen.push(value)
+//   }
+//   return false
+// };
---
+++ src/sudoku.js
@@ -0,0 +1,13 @@
+export default function ThreeBy3 (num1, num2, num3, num4, num5, num6, num7, num8, num9) {
+  this.box1 = num1;
+  this.box2 = num2;
+  this.box3 = num3;
+  this.box4 = num4;
+  this.box5 = num5;
+  this.box6 = num6;
+  this.box7 = num7;
+  this.box8 = num8;
+  this.box9 = num9;
+  this.arrayOfValues = [];
+}
+
