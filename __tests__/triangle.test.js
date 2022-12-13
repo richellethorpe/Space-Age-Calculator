01d858c354e608d7a47627ae03f2feff734d0390
Richelle <richelle.ak@gmail.com>
2022-12-12 23:40:16 -0800

    passed 3rd test

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,36 +1,30 @@
-import ThreeBy3 from './../src/sudoku.js';
+import SudokuCube from './../src/sudoku.js';
 
 
-describe ("ThreeBy3", () => {
+describe ("SudokuCube", () => {
   test("should correctly create an threeby3 object with 9 box values", () =>{
-    const threeby3 = new ThreeBy3(1, 2, 3, 4, 5, 6, 7, 8, 9);
-    expect(threeby3.box1).toEqual(1);
-    expect(threeby3.box2).toEqual(2);
-    expect(threeby3.box3).toEqual(3);
-    expect(threeby3.box4).toEqual(4);
-    expect(threeby3.box5).toEqual(5);
-    expect(threeby3.box6).toEqual(6);
-    expect(threeby3.box7).toEqual(7);
-    expect(threeby3.box8).toEqual(8);
-    expect(threeby3.box9).toEqual(9);
+    const sudokuCube = new SudokuCube(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    expect(sudokuCube.box1).toEqual(1);
+    expect(sudokuCube.box2).toEqual(2);
+    expect(sudokuCube.box3).toEqual(3);
+    expect(sudokuCube.box4).toEqual(4);
+    expect(sudokuCube.box5).toEqual(5);
+    expect(sudokuCube.box6).toEqual(6);
+    expect(sudokuCube.box7).toEqual(7);
+    expect(sudokuCube.box8).toEqual(8);
+    expect(sudokuCube.box9).toEqual(9);
+  })
+
+  test("should correctly create a new array with all the object values", () => {
+    const newCube = new SudokuCube(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    expect(newCube.createArray()).toEqual([ 1,2,3,4,5,6,7,8,9 ])
+  })
+
+  test("should return an error statement if there is a duplicate value in the array", () => {
+    const newCube = new SudokuCube(1, 2, 3, 4, 7, 6, 7, 8, 9);
+    newCube.createArray();
+    expect(newCube.findDuplicates(newCube.arrayOfValues)).toEqual("Not a valid sudoku input")
   })
 })
 
-// ThreeBy3.prototype.createArray = function () {
-//   this.arrayOfValues.splice(0, 8);
-//   let arr = Object.values(this);
-//   arr.pop();
-//   this.arrayOfValues.push(...arr);
-// };
 
-// ThreeBy3.prototype.findDuplicate = function () {
-//   let valuesAlreadySeen = []
-//   for (let i = 0; i < this.arrayOfValues.length; i++) {
-//     let value = this.arrayOfValues[i]
-//     if (valuesAlreadySeen.indexOf(value) !== -1) {
-//       return true
-//     }
-//     valuesAlreadySeen.push(value)
-//   }
-//   return false
-// };
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,6 @@
-export default function ThreeBy3 (num1, num2, num3, num4, num5, num6, num7, num8, num9) {
+export default 
+
+function SudokuCube (num1, num2, num3, num4, num5, num6, num7, num8, num9) {
   this.box1 = num1;
   this.box2 = num2;
   this.box3 = num3;
@@ -10,4 +12,38 @@ export default function ThreeBy3 (num1, num2, num3, num4, num5, num6, num7, num8
   this.box9 = num9;
   this.arrayOfValues = [];
 }
+//let newCube = new SudokuCube (1,2,3,4,5,6,7,8,9);
 
+
+SudokuCube.prototype.createArray = function () {
+  this.arrayOfValues.splice(0, 8);
+  let arr = Object.values(this);
+  arr.pop();
+  this.arrayOfValues.push(...arr);
+  return this.arrayOfValues;
+};
+
+
+SudokuCube.prototype.findDuplicates = function (arr) {
+  console.log(arr.length);
+  for (let i=0; i < arr.length -1; i ++){
+    for (let j = i +1; j < arr.length; j++) {
+      if (arr[i] === arr[j]){
+        return ("Not a valid sudoku input")
+      }
+    }
+  }
+}
+// 
+// newCube.createArray();
+// ThreeBy3.prototype.findDuplicate = function () {
+//   let valuesAlreadySeen = []
+//   for (let i = 0; i < this.arrayOfValues.length; i++) {
+//     let value = this.arrayOfValues[i]
+//     if (valuesAlreadySeen.indexOf(value) !== -1) {
+//       return true
+//     }
+//     valuesAlreadySeen.push(value)
+//   }
+//   return false
+// };
