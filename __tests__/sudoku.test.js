e2a139a61524c8b0983a4ff67638988241dee236
Richelle <richelle.ak@gmail.com>
2022-12-20 19:37:46 -0800

    3rd test for non unique valuess of checkDulplicates function
    Co-authored-by: Carl Sustrich <carl.sustrich@gmail.com>
    dquote> Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>
    dquote> Co-authored-by:  Anahi Ruiz <anahi.sulikey@gmail.com>
    dquote> Co-authored-by: Dani Steely
    <danisteelyart@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,7 +1,9 @@
 import Sudoku from "../src/sudoku.js";
 
 describe('Sudoku', () => {
+
   let newSudoku; 
+  
   beforeEach(() => {
     newSudoku = new Sudoku(1,2,3,4,5,6,7,8,9);
   });
@@ -20,4 +22,10 @@ describe('Sudoku', () => {
   test('it should return true if all values are unique', () => {
     expect(newSudoku.checkDuplicates()).toEqual(true);
   });
+
+  test('it should return false if all values are not unique', () => {
+    newSudoku.space4 = 3;
+    expect(newSudoku.checkDuplicates()).toEqual(false);
+  })
+
  });
