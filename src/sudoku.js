4c898105b67698a78a0d301a2e723f2c8750b216
Richelle <richelle.ak@gmail.com>
2022-12-20 19:08:12 -0800

    first test, passed
    Co-authored-by: Carl Sustrich <carl.sustrich@gmail.com>
    dquote> Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>
    dquote> Co-authored-by:  Anahi Ruiz <anahi.sulikey@gmail.com>
    dquote> Co-authored-by: Dani Steely
    <danisteelyart@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,6 +1,14 @@
 export default class Sudoku {
-  constructor() {
-
+  constructor(space1, space2, space3, space4, space5, space6, space7, space8, space9) {
+    this.space1 = space1;
+    this.space2 = space2;
+    this.space3 = space3;
+    this.space4 = space4;
+    this.space5 = space5;
+    this.space6 = space6;
+    this.space7 = space7;
+    this.space8 = space8;
+    this.space9 = space9;
   }
 }
 
