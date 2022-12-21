04346d912582f295e26dcb4d0175e0648bb84830
Richelle <richelle.ak@gmail.com>
2022-12-20 18:47:07 -0800

    add basic repo
    Co-authored-by: Carl Sustrich <carl.sustrich@gmail.com>
    Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>
    Co-authored-by:  Anahi Ruiz <anahi.sulikey@gmail.com>
    Co-authored-by: Dani Steely
    <danisteelyart@gmail.com>

---
+++ __tests__/sudoku.test.js
@@ -0,0 +1,13 @@
+
+// import Haiku from "../src/euler";
+// // Blowing from the west
+// // Fallen leaves gather
+// // In the east .
+// describe ("Haiku", () => {
+//   test("should correctly return a haiku object with three lines", () => {
+//     const newHaiku = new Haiku("Blowing from the west","Fallen leaves gather","In the east");
+//     expect(newHaiku.line1).toEqual("Blowing from the west");
+//     expect(newHaiku.line2).toEqual("Fallen leaves gather");
+//     expect(newHaiku.line3).toEqual("In the east");
+//   })
+// })
