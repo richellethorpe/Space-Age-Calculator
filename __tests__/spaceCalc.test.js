c2eef6fc09a724affbdffd0489acb8ad14c17e1c
Richelle <richelle.ak@gmail.com>
2022-12-21 19:14:58 -0800

    add sencond test

--- __tests__/spaceCalc.test.js
+++ __tests__/spaceCalc.test.js
@@ -1,11 +1,24 @@
+//rrmt
 
-
+import { beforeEach } from "node:test";
 import SpaceCalc from "../src/spaceCalc";
 
+
 describe ("SpaceCalc", () => {
+  let newSpaceCalc;
+
+  beforeEach(() =>{
+    newSpaceCalc = new SpaceCalc(36);
+  });
+
   test("should correctly return a space calc object with one age value in earth years", () => {
-    const newSpaceCalc = new SpaceCalc(36);
+    
     expect(newSpaceCalc.earthAge).toEqual(36);
+  })
+
+  test("should correctly return a space calc object with age value in mercury years", () => {
+    newSpaceCalc.findMercuryAge();
+    expect(newSpaceCalc.mercuryAge).toEqual(105);
   })
 })
 
